4a5fb10df107d47e6773d75656b2546e259467ea
Jonathan Liu <liu.z.jonathan@gmail.com>
2020-10-11 13:13:17 -0700

    Initialized project

---
+++ webapp/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>very cool!</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
