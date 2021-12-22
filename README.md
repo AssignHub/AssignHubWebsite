698a7d65c66634df60e83147886e1c8f17b856d5
Jonathan Liu <liu.z.jonathan@gmail.com>
2021-12-22 00:43:17 -0600

    created server deployment script

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ A live demo of the website can be found here: [DEMO](https://assignhub.github.io
 ## Deployment
 ### Frontend
 1. Make sure you are on the `master` branch
-2. Run `deploy_frontend.sh SERVER_HOST AWS_KEY_LOCATION`
+2. Run `./deploy_scripts/deploy_frontend.sh SERVER_HOST AWS_KEY_LOCATION`
 ### Backend
-1. `sudo pm2 restart server`
+1. Make sure you are on the `master` branch
+2. Run `./deploy_scripts/deploy_server.sh SERVER_HOST AWS_KEY_LOCATION`
--- deploy_scripts/shared.sh
+++ deploy_scripts/shared.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# shared.sh contains the scripting stuff that is shared between the deploy script for frontend and backend
+# Such shared stuff includes: checking the arguments are correct and that current branch is set correctly
+
 # Check arguments
 if [ $# -ne 2 ]
 then
-  echo "ERROR: Incorrect number of arguments! USAGE: deploy_frontend.sh SERVER_HOST AWS_KEY_LOCATION"
+  SCRIPT_NAME="$( grep -o '\/[^\/]*$' <<< $0 )"
+  echo "ERROR: Incorrect number of arguments! USAGE: .$SCRIPT_NAME SERVER_HOST AWS_KEY_LOCATION"
   exit 1
 fi
 
@@ -11,7 +15,6 @@ SERVER_HOST="$1"
 AWS_KEY_LOCATION="$( pwd )/$2"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEPLOY_BRANCH="master" # The branch to deploy
-DIST_FOLDER_SERVER_LOCATION="~/dist" #"/assignhub/frontend/dist/" # The location of the dist folder on the server
 
 # Check if aws key file exists
 if test ! -f "$AWS_KEY_LOCATION"
@@ -21,26 +24,10 @@ then
 fi
 
 # Check if on correct branch
-cd $SCRIPT_DIR/frontend
+cd $SCRIPT_DIR/../
 CUR_BRANCH="$( git rev-parse --abbrev-ref HEAD )"
 if [ "$CUR_BRANCH" != "$DEPLOY_BRANCH" ]
 then
   echo "ERROR: Current branch is not \"$DEPLOY_BRANCH\". Please switch the current branch to \"$DEPLOY_BRANCH\" before deploying."
   exit 1
 fi
-
-# Build frontend
-echo "Building frontend..."
-npm install
-npm run build
-
-# Delete old build 
-echo -n "Deleting old dist folder..."
-ssh $SERVER_HOST -i $AWS_KEY_LOCATION "rm -rf $DIST_FOLDER_SERVER_LOCATION"
-echo "Done!"
-
-# Transfer build to server
-echo "Transferring build to server..."
-scp -i $AWS_KEY_LOCATION -r $SCRIPT_DIR/frontend/dist $SERVER_HOST:$DIST_FOLDER_SERVER_LOCATION
-
-echo "Done!"
