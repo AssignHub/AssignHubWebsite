133165fa45513883c2c72dc42c25f62f3f486067
Jonathan Liu <liu.z.jonathan@gmail.com>
2021-12-29 00:07:09 -0800

    sudo fix

--- deploy_scripts/deploy_server.sh
+++ deploy_scripts/deploy_server.sh
@@ -9,5 +9,5 @@ ROOT_FOLDER_SERVER_LOCATION="/assignhub"
 
 # Git pull on server, npm install, and restart server process
 echo "Deploying server..."
-ssh $SERVER_HOST -i $AWS_KEY_LOCATION "cd $ROOT_FOLDER_SERVER_LOCATION && sudo git stash && sudo git pull && cd server && npm install && sudo pm2 restart server"
+ssh $SERVER_HOST -i $AWS_KEY_LOCATION "cd $ROOT_FOLDER_SERVER_LOCATION && sudo git stash && sudo git pull && cd server && sudo npm install && sudo pm2 restart server"
 echo "Done!"
