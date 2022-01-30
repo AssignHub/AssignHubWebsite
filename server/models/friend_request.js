78c1d9f15dd1331b00e99e612304380299ea7182
Tony Xin <xyytonyxin@gmail.com>
2022-01-30 15:21:28 -0800

    Finalized emailing

--- server/models/friend_request.js
+++ server/models/friend_request.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const friendRequestSchema = new mongoose.Schema({
   timestamp: { type: Date, required: true },
-  lastReminded: { type: Date, required: true },
   from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 },
