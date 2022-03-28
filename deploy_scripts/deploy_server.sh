fc202f88ef3ae3f0791777bda6811fcbe3a0f1c0
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-03-27 21:52:38 -0700

    reverse script order

--- deploy_scripts/deploy_server.sh
+++ deploy_scripts/deploy_server.sh
@@ -7,11 +7,11 @@ source $SCRIPT_DIR/shared.sh
 # Define variables
 ROOT_FOLDER_SERVER_LOCATION="/assignhub"
 
+# SCP some miscellaneous gitignored files
+scp -i $AWS_KEY_LOCATION -r $SCRIPT_DIR/../server/schools/waldorf/allowed_emails.json $SERVER_HOST:~/
+ssh $SERVER_HOST -i $AWS_KEY_LOCATION "sudo mv ~/allowed_emails.json $ROOT_FOLDER_SERVER_LOCATION/server/schools/waldorf/"
+
 # Git pull on server, npm install, and restart server process
 echo "Deploying server..."
 ssh $SERVER_HOST -i $AWS_KEY_LOCATION "cd $ROOT_FOLDER_SERVER_LOCATION && sudo git stash && sudo git pull && cd server && sudo npm install && sudo pm2 restart server"
 echo "Done!"
-
-# SCP some miscellaneous gitignored files
-scp -i $AWS_KEY_LOCATION -r $SCRIPT_DIR/../server/schools/waldorf/allowed_emails.json $SERVER_HOST:~/
-ssh $SERVER_HOST -i $AWS_KEY_LOCATION "sudo mv ~/allowed_emails.json $ROOT_FOLDER_SERVER_LOCATION/server/schools/waldorf/"
