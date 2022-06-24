2d81b3e135a4870f511bf60ea46984eb9ce1ef10
Tony Xin <xyytonyxin@gmail.com>
2022-06-24 17:01:42 -0500

    Initial classes blank

---
+++ frontend/src/components/ClassTodos.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import type { Class } from '@/types'
+
+const props = defineProps({
+    _class: { type: Object as () => Class, required: true }
+})
+
+const headerStyle = computed(() => {
+    return {
+        backgroundColor: props._class.color
+    }
+})
+
+const bodyStyle = computed(() => {
+    return {
+        backgroundColor: `${props._class.color}80`
+    }
+})
+</script>
+
+<template>
+    <div class="tw-h-64 tw-rounded-lg" :style="bodyStyle">
+        <div class="tw-flex tw-items-center tw-w-full tw-text-white tw-rounded-t-lg tw-p-2 tw-relative"
+            :style="headerStyle">
+            <div class="tw-ml-2">
+                {{ props._class.courseId }}
+
+            </div>
+            <v-btn class="tw-absolute tw-right-0 tw-top-0" flat icon size="small">
+                <v-icon>mdi-plus</v-icon>
+            </v-btn>
+
+        </div>
+    </div>
+</template>
--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,5 +1,38 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import ClassTodos from './ClassTodos.vue'
+
+const classes = ref([
+  {
+    _id: '1234',
+    courseId: 'CSCI-170',
+    name: 'Discrete Methods in Computer Science',
+    color: '#6b7280',
+  },
+  {
+    _id: '123',
+    courseId: 'CSCI-103',
+    name: 'Introduction to Programming',
+    color: '#ef4444',
+  },
+  {
+    _id: '12',
+    courseId: 'BUAD-302',
+    name: 'Business Communications',
+    color: '#22c55e',
+  }
+])
 </script>
 
 <template>
+    <div class="tw-p-6 tw-max-w-full">
+        <div class="tw-text-xl tw-font-semibold tw-mb-4">Todo</div>
+        <div class="tw-grid tw-grid-cols-2 tw-gap-2">
+          <ClassTodos 
+            v-for="c in classes"
+            :key="c._id"
+            :_class="c"
+          />
+        </div>
+    </div>
 </template>
