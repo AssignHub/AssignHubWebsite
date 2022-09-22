b26e934805fe9723beeea77bbdf2ddd48b39f173
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-09-22 00:25:37 -0700

    upgraded vue and tailwind stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
