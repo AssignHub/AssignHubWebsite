950c09166d79fc6fcd0f6c9ef36972e3177d9f6e
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-09-26 15:46:40 -0700

    added beta tag

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -5,7 +5,14 @@
       <v-col cols="12" sm="auto" class="top-center">
         <v-card color="" class="px-16 pb-4 pt-10">
           <v-card-text class="text-center">
-            <v-img src="@/assets/logo.png" width="300px" transition="scale-transition" contain class="mx-auto mb-10"></v-img>
+            <div class="tw-relative">
+              <v-img src="@/assets/logo.png" width="300px" transition="scale-transition" contain class="mx-auto mb-10"></v-img>
+              <v-chip 
+                class="tw-absolute tw-bottom-4 -tw-right-4 tw-font-semibold"
+                label
+                color="primary"
+              >BETA</v-chip>
+            </div>
             <div id="sign-in-btn" style="display: inline-block;"></div>
             <div class="text-caption">
               Make sure to use your school email address to sign in!
