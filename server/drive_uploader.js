f5736cce3ee6d97223c05a85784643de347c282e
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-10-01 16:02:38 -0700

    fixed syllabus parsing

--- server/drive_uploader.js
+++ server/drive_uploader.js
@@ -3,20 +3,21 @@ const { google } = require('googleapis')
 const { Readable } = require('stream')
 const { sendMessage } = reqlib('discord_bot')
 const { SyllabusStatus } = reqlib('constants')
+const { emitToUser } = reqlib('/websockets')
 const Class = reqlib('models/class')
 require('dotenv').config()
 
 exports.uploadSyllabus = async (user, classId, files, comment) => {
-  const client = google.auth.fromJSON({
-    type: 'authorized_user',
-    client_id: process.env.DRIVE_UPLOADER_CLIENT_ID,
-    client_secret: process.env.DRIVE_UPLOADER_CLIENT_SECRET,
-    refresh_token: process.env.DRIVE_UPLOADER_REFRESH_TOKEN,
-  })
-
-  const drive = google.drive({version: 'v3', auth: client})
-
   try {
+    const client = google.auth.fromJSON({
+      type: 'authorized_user',
+      client_id: process.env.DRIVE_UPLOADER_CLIENT_ID,
+      client_secret: process.env.DRIVE_UPLOADER_CLIENT_SECRET,
+      refresh_token: process.env.DRIVE_UPLOADER_REFRESH_TOKEN,
+    })
+
+    const drive = google.drive({version: 'v3', auth: client})
+
     const classFolderId = await createFolderIfNotExist(drive, process.env.DRIVE_UPLOADER_ASSIGNHUB_FOLDER_ID, classId)
     const userFolderId = await createFolderIfNotExist(drive, classFolderId, user.email)
 
@@ -90,5 +91,10 @@ const createFolderIfNotExist = async (drive, parentFolderId, folderName) => {
 }
 
 exports.setSyllabusStatus = async (classId, syllabusStatus) => {
-  await Class.findByIdAndUpdate(classId, { syllabusStatus })
+  const _class = await Class.findByIdAndUpdate(classId, { syllabusStatus })
+
+  const users = await _class.findMembers()
+  for (const user of users) {
+    emitToUser(user._id, 'updateClass', { classId: classId, updatedData: { syllabusStatus } })
+  }
 }
