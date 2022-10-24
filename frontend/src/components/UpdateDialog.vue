262f62e3c533fbdb884bb3070cd05a646f841f2a
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-10-24 01:14:06 -0700

    pwa support

---
+++ frontend/src/components/UpdateDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog
+    :value="value"
+    @input="v => $emit('input', v)"
+    persistent
+    width="300"
+  >
+    <v-card>
+      <v-card-title>Update available</v-card-title>
+      <v-card-text>
+        A new update for AssignHub is available, would you like to reload the page?
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text @click="close">Cancel</v-btn>
+        <v-btn color="primary" @click="reload">Reload</v-btn> 
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'UpdateDialog',
+
+  props: {
+    value: { type: Boolean },
+  },
+
+  methods: {
+    close() {
+      this.$emit('input', false)
+    },
+    reload() {
+      this.$emit('accept')
+      this.close()
+    },
+  },
+}
+</script>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,5 +10,22 @@ module.exports = {
         'introJs': ['intro.js']
       })
     ]
-  }
+  },
+  pwa: {
+    name: 'AssignHub',
+    themeColor: '#5E76BC',
+    msTileColor: '#5E76BC',
+    appleMobileWebAppCache: 'yes',
+    manifestOptions: {
+      background_color: '#5E76BC',
+    },
+    iconPaths: {
+      faviconSVG: null,
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: null,
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+  },
 }
